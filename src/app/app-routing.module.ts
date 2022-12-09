8e1eac0c80faaa6c05dd898c3fdfcc9edbc801d8
Gaspar Santillan <gazz9ar@gmail.com>
2022-12-09 12:52:26 -0300

    fix deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
 	},
 	{ path: "", redirectTo: "home", pathMatch: "full" },
 	{ path: "**",  redirectTo: "home"},
-];
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
